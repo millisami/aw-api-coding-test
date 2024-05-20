25c33ae883f28876cdc083298fe393d0d5a22dd5
Sachin <millisami@gmail.com>
2024-05-20 12:08:48 +0545

    Auth controller added for signin

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,25 @@
+module Api::V1
+  class AuthController < ApplicationController
+    skip_before_action :authenticated!, only: [:signin]
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    def signin
+      @user = User.find_by!(email: signin_params[:email])
+      if @user.authenticate(signin_params[:password])
+        render json: V1::UserSerializer.new(@user), status: :accepted
+      else
+        render json: { error: 'Incorrect password' }, status: :unauthorized
+      end
+    end
+
+    private
+    
+    def signin_params
+      params.permit(:email, :password)
+    end
+
+    def record_not_found(e)
+      render json: { error: "Email doesn not exist" }, status: :unauthorized
+    end
+  end
+end
---
+++ spec/requests/api/v1/auth_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Auth", type: :request do
+  describe "POST /auth/signin" do
+    let!(:user) { create(:user, email: 'john@example.com', password: 'sekret')}
+    
+    it "authenticates and return a token" do
+      post "/api/v1/auth/signin", params: { email: user.email, password: 'sekret' }
+
+      expect(response).to have_http_status(:accepted)
+      expect(json['id']).to eq("#{User.last.id}")
+      expect(json['type']).to eq('user')
+      expect(json['attributes']['email']).to eq('john@example.com')
+      expect(json['attributes']['token']).to eq(AuthTokenService.call(user.id))
+    end
+
+    it "returns error when email does not exist" do
+      post "/api/v1/auth/signin", params: { email: 'noemail@host.com', password: 'sekret' }
+
+      expect(response).to have_http_status(:unauthorized)
+      expect(response.body).to eq({
+        'error': "Email doesn not exist"
+      }.to_json)
+    end
+
+    it "returns error when password does not match" do
+      post "/api/v1/auth/signin", params: { email: user.email, password: 'wrong' }
+
+      expect(response).to have_http_status(:unauthorized)
+      expect(response.body).to eq({
+        'error': "Incorrect password"
+      }.to_json)
+    end
+  end
+end
